ce2f857f55e7bfd0305d991b893dde3eb40bbf10
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2020-06-10 23:26:13 +0200

    Added DISCORD_TOKEN variable

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
+import os
 import discord
 from discord.ext import commands
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 
 client = commands.Bot(command_prefix='.')
 
@@ -8,4 +11,4 @@ client = commands.Bot(command_prefix='.')
 async def on_ready():
     print('Bot is ready!')
 
-client.run('NzIwMzgzMDk5Njg2NjE3MTE4.XuFLWg.la17vTnNnpYndTCban1bgxwWwRE')
+client.run(os.environ['DISCORD_TOKEN'])
