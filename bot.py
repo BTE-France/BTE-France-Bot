9120a09d36bb308ebb5f559c20dee3022a3f4835
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2020-06-10 23:11:42 +0200

    Added token

--- bot.py
+++ bot.py
@@ -7,3 +7,5 @@ client = commands.Bot(command_prefix='.')
 @client.event
 async def on_ready():
     print('Bot is ready!')
+
+client.run('NzIwMzgzMDk5Njg2NjE3MTE4.XuFLWg.la17vTnNnpYndTCban1bgxwWwRE')
