644e5b89ba05c8eabccd9f83c0c147bb2b451193
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2020-07-24 12:56:50 +0200

    Added footer to .help embed

--- cogs/help.py
+++ cogs/help.py
@@ -12,6 +12,7 @@ class Help(commands.Cog):
             await ctx.send(f"{ctx.author.mention}, regarde tes MPs! :mailbox:")
         embed = discord.Embed(title='Help!', colour=discord.Colour(0xFF0000))
         embed.set_thumbnail(url="https://cdn.discordapp.com/icons/694003889506091100/a_c40ba19cfcfbb9db5f5060e85f6539cf.png?size=128")
+        embed.set_footer(text='Contacte @mAxYoLo01#4491 si tu trouves des bugs / suggestions!', icon_url='https://cdn.discordapp.com/icons/694003889506091100/a_c40ba19cfcfbb9db5f5060e85f6539cf.png?size=128')
         for cog in [self.client.get_cog(cogs) for cogs in self.client.cogs]:
             for command in cog.get_commands():
                 if command.checks:
