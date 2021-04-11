2598602416069cbf1756bcd72680ff1cb206b731
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2021-04-11 16:08:16 +0200

    Added .lire command

--- cogs/random_commands.py
+++ cogs/random_commands.py
@@ -32,6 +32,10 @@ class RandomCommands(commands.Cog):
             colour=discord.Colour(0x0800FF),
             description="https://www.facebook.com/Build-The-Earth-France-113380800556340"
         )
+        self.rules_embed = discord.Embed(
+            description=f"As-tu bien lu les salons {self.client.get_channel(810512822928670780).mention} et {self.client.get_channel(821291710993661992).mention}?",
+            colour=discord.Colour(0xFF0000)
+        )
         self.map_embed.set_thumbnail(url=self.thumbnail_url)
         self.youtube_embed.set_thumbnail(url=self.thumbnail_url)
         self.twitter_embed.set_thumbnail(url=self.thumbnail_url)
@@ -70,6 +74,10 @@ class RandomCommands(commands.Cog):
     @commands.command(brief='Ban Image')
     async def ban(self, ctx):
         await ctx.send('https://i.imgur.com/TnjvTKh.png')
+
+    @commands.command(brief='Read the rules')
+    async def lire(self, ctx):
+        await ctx.send(embed=self.rules_embed)
 
 
 def setup(client):
