c7320e9a681175eb4fbea2a4159a793f7719e60a
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2021-05-10 22:21:57 +0200

    Added .ip command

--- cogs/random_commands.py
+++ cogs/random_commands.py
@@ -10,77 +10,92 @@ class RandomCommands(commands.Cog):
         self.map_embed = discord.Embed(
             title="Map du Projet BTE",
             colour=discord.Colour(0x00FF00),
-            description="https://buildtheearth.net/map"
+            description="https://buildtheearth.net/map",
         )
         self.youtube_embed = discord.Embed(
             title="YouTube de BTE: France",
             colour=discord.Colour(0xFF0000),
-            description="https://www.youtube.com/c/BTEFrance"
+            description="https://www.youtube.com/c/BTEFrance",
         )
         self.twitter_embed = discord.Embed(
             title="Twitter de BTE: France",
             colour=discord.Colour(0x0000FF),
-            description="https://twitter.com/BuildFr"
+            description="https://twitter.com/BuildFr",
         )
         self.instagram_embed = discord.Embed(
             title="Instagram de BTE: France",
             colour=discord.Colour(0xFFFF00),
-            description="https://www.instagram.com/bte_france/"
+            description="https://www.instagram.com/bte_france/",
         )
         self.facebook_embed = discord.Embed(
             title="Facebook de BTE: France",
             colour=discord.Colour(0x0800FF),
-            description="https://www.facebook.com/Build-The-Earth-France-113380800556340"
+            description="https://www.facebook.com/Build-The-Earth-France-113380800556340",
+        )
+        self.ip_embed = discord.Embed(
+            title="IPs de BTE: France",
+            colour=discord.Colour(0x0800FF),
+            description=":arrow_right: Version Java: `buildtheearth.net`, puis `/bt FR` quand vous êtes dans le lobby\n:arrow_right: Version Bedrock: `bedrock.buildtheearth.net`, port `19132`.",
         )
         self.map_embed.set_thumbnail(url=self.thumbnail_url)
         self.youtube_embed.set_thumbnail(url=self.thumbnail_url)
         self.twitter_embed.set_thumbnail(url=self.thumbnail_url)
         self.instagram_embed.set_thumbnail(url=self.thumbnail_url)
         self.facebook_embed.set_thumbnail(url=self.thumbnail_url)
+        self.ip_embed.set_thumbnail(url=self.thumbnail_url)
 
-    @commands.command(brief='Donne la latence du bot')
+    @commands.command(brief="Donne la latence du bot")
     async def ping(self, ctx):
-        await ctx.send(f'Pong! {round(self.client.latency*1000)}ms')
+        await ctx.send(f"Pong! {round(self.client.latency*1000)}ms")
 
-    @commands.command(aliases=['purge'], brief='Supprime les x derniers messages')
-    @commands.check_any(commands.is_owner(), commands.has_permissions(manage_roles=True, manage_channels=True))
+    @commands.command(aliases=["purge"], brief="Supprime les x derniers messages")
+    @commands.check_any(
+        commands.is_owner(),
+        commands.has_permissions(manage_roles=True, manage_channels=True),
+    )
     async def clear(self, ctx, number=1):
         await ctx.channel.purge(limit=number + 1)
 
-    @commands.command(brief='Lien de la Map BTE')
+    @commands.command(brief="Lien de la Map BTE")
     async def map(self, ctx):
         await ctx.send(embed=self.map_embed)
 
-    @commands.command(brief='Lien YouTube', aliases=['ytb', 'yt'])
+    @commands.command(brief="Lien YouTube", aliases=["ytb", "yt"])
     async def youtube(self, ctx):
         await ctx.send(embed=self.youtube_embed)
 
-    @commands.command(brief='Lien Twitter', aliases=['twi', 'tw'])
+    @commands.command(brief="Lien Twitter", aliases=["twi", "tw"])
     async def twitter(self, ctx):
         await ctx.send(embed=self.twitter_embed)
 
-    @commands.command(brief='Lien Instagram', aliases=['insta'])
+    @commands.command(brief="Lien Instagram", aliases=["insta"])
     async def instagram(self, ctx):
         await ctx.send(embed=self.instagram_embed)
 
-    @commands.command(brief='Lien Facebook', aliases=['fb'])
+    @commands.command(brief="Lien Facebook", aliases=["fb"])
     async def facebook(self, ctx):
         await ctx.send(embed=self.facebook_embed)
 
-    @commands.command(brief='Ban Image')
+    @commands.command(brief="Ban Image")
     async def ban(self, ctx):
-        await ctx.send('https://i.imgur.com/TnjvTKh.png')
+        await ctx.send("https://i.imgur.com/TnjvTKh.png")
 
-    @commands.command(brief='Stonks GIF')
+    @commands.command(brief="Stonks GIF")
     async def stonks(self, ctx):
-        await ctx.send('https://media2.giphy.com/media/YnkMcHgNIMW4Yfmjxr/giphy.gif')
+        await ctx.send("https://media2.giphy.com/media/YnkMcHgNIMW4Yfmjxr/giphy.gif")
 
-    @commands.command(brief='Read the rules')
+    @commands.command(brief="Read the rules")
     async def lire(self, ctx):
-        await ctx.send(embed=discord.Embed(
-            description=f"As-tu bien lu le salon {self.client.get_channel(variables.comment_rejoindre_channel).mention}?",
-            colour=discord.Colour(0xFF0000)
-        ))
+        await ctx.send(
+            embed=discord.Embed(
+                description=f"As-tu bien lu le salon {self.client.get_channel(variables.comment_rejoindre_channel).mention}?",
+                colour=discord.Colour(0xFF0000),
+            )
+        )
+
+    @commands.command(brief="Liste IPs")
+    async def ip(self, ctx):
+        await ctx.send(embed=self.ip_embed)
 
 
 def setup(client):
