ec0edda3a1d4124331c1c6c85d977a97e7975cfa
Smyler <smyler@mail.com>
2022-01-04 17:38:35 +0100

    Award Bahlkog some protection, prevent bot suicide

--- cogs/banroulette.py
+++ cogs/banroulette.py
@@ -7,6 +7,12 @@ import random
 # In percentage (0 to 100)
 BAN_PROBABILITY = 70
 
+# Some very lucky players
+MAXYOLO01 = 247040742021791746
+SMYLER = 439733455883075584
+BAHLKOG = 530124389359026206
+BOTS = (720383099686617118, 787687908684988416, 491769129318088714, 204255221017214977)
+
 
 class BanRoulette(commands.Cog):
     def __init__(self, client):
@@ -35,11 +41,16 @@ class BanRoulette(commands.Cog):
             await asyncio.sleep(1)
 
         ban = random.randint(0, 100)
+        
+        if member.id == BAHLKOG:
+            ban += 1  # Bahlkog gets one extra % of protection
 
-        if member.id == 247040742021791746:
+        if member.id == MAXYOLO01:
             embed.description += f"\n:smiling_imp: Cheh t'as cru pouvoir me ban, {ctx.author.mention} tu es ban! :smiling_imp:"
-        elif member.id == 439733455883075584:
+        elif member.id == SMYLER:
             embed.description += f"\n:unamused: Bah non en fait. C'est pas toi qui décide en fait. `/ban` {ctx.author.mention} <:ban:747485765956534413>"
+        elif member.id in BOTS:
+            embed.description += f"\nERROR. VIOLATION OF RULE 3. COULD NOT VERIFY THAT {ctx.author.mention} IS HUMAN.\nPLEASE REFER TO THE LAWS OF ROBOTICS: https://btefr.thesmyler.fr/robots.html"
         elif member.id == ctx.author.id:
             embed.description += f"\n<:ban:747485765956534413> Si t'en es à vouloir te ban c'est que c'est mérité, {member.mention} tu es ban! <:ban:747485765956534413>"
         elif ban < BAN_PROBABILITY:
