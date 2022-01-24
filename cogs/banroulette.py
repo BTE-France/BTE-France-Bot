58c8026f392ee9c6183f55e3705e1d781feafed4
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-01-24 10:18:03 +0100

    Changed banroulette order

--- cogs/banroulette.py
+++ cogs/banroulette.py
@@ -11,7 +11,6 @@ BAN_PROBABILITY = 70
 MAXYOLO01 = 247040742021791746
 SMYLER = 439733455883075584
 BAHLKOG = 530124389359026206
-BOTS = (720383099686617118, 787687908684988416, 491769129318088714, 204255221017214977)
 
 
 class BanRoulette(commands.Cog):
@@ -41,18 +40,18 @@ class BanRoulette(commands.Cog):
             await asyncio.sleep(1)
 
         ban = random.randint(0, 100)
-        
+
         if member.id == BAHLKOG:
             ban += 1  # Bahlkog gets one extra % of protection
 
-        if member.id == MAXYOLO01:
+        if member.id == ctx.author.id:
+            embed.description += f"\n<:ban:747485765956534413> Si t'en es à vouloir te ban c'est que c'est mérité, {member.mention} tu es ban! <:ban:747485765956534413>"
+        elif member.id == MAXYOLO01:
             embed.description += f"\n:smiling_imp: Cheh t'as cru pouvoir me ban, {ctx.author.mention} tu es ban! :smiling_imp:"
         elif member.id == SMYLER:
             embed.description += f"\n:unamused: Bah non en fait. C'est pas toi qui décide en fait. `/ban` {ctx.author.mention} <:ban:747485765956534413>"
-        elif member.id in BOTS:
+        elif member.bot:
             embed.description += f"\nERROR. VIOLATION OF RULE 3. COULD NOT VERIFY THAT {ctx.author.mention} IS HUMAN.\nPLEASE REFER TO THE LAWS OF ROBOTICS: https://btefr.thesmyler.fr/robots.html"
-        elif member.id == ctx.author.id:
-            embed.description += f"\n<:ban:747485765956534413> Si t'en es à vouloir te ban c'est que c'est mérité, {member.mention} tu es ban! <:ban:747485765956534413>"
         elif ban < BAN_PROBABILITY:
             embed.description += f"\n<:ban:747485765956534413> La sentence est irrévocable, {member.mention} tu es ban! <:ban:747485765956534413>"
         else:
