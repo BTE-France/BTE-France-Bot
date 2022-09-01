bddcf87c36b28313872522238f8a6f7b1f2c3932
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-09-01 20:47:27 +0100

    show all players in /mc, + differentiate staff

--- cogs/mcserv.py
+++ cogs/mcserv.py
@@ -5,13 +5,43 @@ import interactions
 import socket
 
 
-# List of servers to get the status from.
-servers = [
-    {
-        "ip": "btefr.thesmyler.fr:7000",
-        "desc": "Serveur Moddé 1.12 - IP: buildtheearth.net, /bt FR",
-    }
+STAFF_USERNAMES = [
+    "lennah_",
+    "*maxdrey49",
+    "bahlkog",
+    "l0pal",
+    "lucas_build",
+    "madin0",
+    "maxyolo01",
+    "raphinoj",
+    "sandpaper_dreams",
+    "serenityjane",
+    "weelly_",
+    "eklo",
+    "egan83",
+    "goul20",
+    "babyoda_",
+    "maxdrey",
+    "smyler_",
+    "thesmyler",
+    "__elfi__",
+    "litverkzad",
+    "picopioche",
+    "repsic",
+    "zxsp3ctrom",
+    "tytarex",
+    "lclc",
+    "azguendare",
 ]
+
+# List of servers to get the status from.
+SERVERS = [
+    ("btefr.thesmyler.fr:7000", "Serveur Moddé 1.12 - IP: buildtheearth.net, /bt FR")
+]
+
+
+def escape_markdown(string: str) -> str:
+    return string.replace("_", r"\_").replace("*", r"\*")  # Escape markdown symbols that could be present in players' names
 
 
 class MCServ(interactions.Extension):
@@ -21,36 +51,33 @@ class MCServ(interactions.Extension):
     @interactions.extension_command(name="mc", description="Statut du serveur Minecraft", scope=server)
     async def mc(self, ctx: interactions.CommandContext):
         fields = []
-        for server_dict in servers:
-
+        for _server in SERVERS:
+            ip, desc = _server
             try:
-                status = JavaServer.lookup(server_dict["ip"]).status()
+                query = await (await JavaServer.async_lookup(ip)).async_query()
 
             except (ConnectionRefusedError, socket.timeout):
                 embed_value = ":x: Serveur hors ligne!"
-                embed_value += "\n\n_ _" if server_dict == servers[0] else ""
-                fields.append((server_dict["desc"], embed_value, False))
+                embed_value += "\n\n_ _" if _server == SERVERS[0] else ""
+                fields.append((desc, embed_value, False))
 
             else:
-                online_players = status.players.online
-                sample = sorted([p.name for p in status.players.sample]) if status.players.sample is not None else []
-
-                sample_title = "Joueurs Connectés" if online_players != 1 else "Joueur Connecté"
-
-                sample_txt = ""
-                if online_players != 0:
-                    diff = online_players - len(sample)
-                    if diff > 0:
-                        sample_txt = ", ".join(sample) + f" et {diff} autre"
-                        sample_txt += "." if diff == 1 else "s."
-                    elif online_players == 1:
-                        sample_txt = sample[0]
+                staff, players = [], []
+                for player in query.players.names:
+                    if player.lower() in STAFF_USERNAMES:
+                        staff.append(f"**{escape_markdown(player)}**")
                     else:
-                        sample_txt = ", ".join(sample[:-1]) + " et " + sample[-1]
-                sample_txt = sample_txt.replace("_", r"\_").replace("*", r"\*")  # Escape markdown symbols that could be present in players' names
+                        players.append(escape_markdown(player))
 
-                embed_value = f":white_check_mark: Serveur en ligne!\n\n**{online_players} {sample_title}**\n" + sample_txt
-                fields.append((server_dict["desc"], embed_value, False))
+                staff.sort()
+                players.sort()
+
+                len_players = len(staff) + len(players)
+                title = f"{len_players} {'Joueurs Connectés' if len_players != 1 else 'Joueur Connecté'}"
+
+                txt = ", ".join(staff + players)
+                value = f":white_check_mark: Serveur en ligne!\n\n**{title}**\n" + txt
+                fields.append((desc, value, False))
 
         await ctx.send(embeds=create_embed(
             title="**Statut du serveur Minecraft BTE France**",
