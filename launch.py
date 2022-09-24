256c7a92868dbac517b3ef2b5445c450b5bf8a3d
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-09-24 16:09:30 +0100

    remove useless --sync script argument

--- launch.py
+++ launch.py
@@ -1,18 +1,11 @@
-from argparse import ArgumentParser
 import interactions
 import logging
 import os
 
 logging.basicConfig(format='[%(levelname)s] %(message)s')
 
-parser = ArgumentParser()
-parser.add_argument('-s', '--sync', action='store_true', help="synchronize slash commands on the Discord API")
-args = parser.parse_args()
-disable_sync = False if args.sync else True
-
 bot = interactions.Client(
     token=os.environ["DISCORD_TOKEN"],
-    disable_sync=disable_sync,
     presence=interactions.ClientPresence(
         status=interactions.StatusType.ONLINE,
         activities=[interactions.PresenceActivity(
