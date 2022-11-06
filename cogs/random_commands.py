55a51ac7351a546f00e9899b49f319023689f3f5
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-11-06 17:17:41 +0000

    update ban listener + lib updated to 4.3.4

--- cogs/random_commands.py
+++ cogs/random_commands.py
@@ -1,4 +1,3 @@
-import asyncio
 from utils.embed import create_embed, create_info_embed
 from variables import server, comment_rejoindre_channel, logs_channel
 import interactions
@@ -8,6 +7,11 @@ import string
 class RandomCommands(interactions.Extension):
     def __init__(self, client: interactions.Client):
         self.client: interactions.Client = client
+
+    @interactions.extension_listener()
+    async def on_start(self):
+        self.guild: interactions.Guild = await interactions.get(self.client, interactions.Guild, object_id=server)
+        self.logs_channel = await interactions.get(self.client, interactions.Channel, object_id=logs_channel)
 
     @interactions.extension_command(name="clear", description="Supprimer les x derniers messages", default_member_permissions=interactions.Permissions.MANAGE_MESSAGES)
     @interactions.option("Nombre de messages à supprimer")
@@ -44,16 +48,16 @@ class RandomCommands(interactions.Extension):
         ))
 
     @interactions.extension_listener()
-    async def on_guild_ban_add(self, guild_ban: interactions.GuildBan):
-        await asyncio.sleep(3)  # Leave some time for the audit log to be updated
-        audit_log = await self.client._http.get_guild_auditlog(server, action_type=22, limit=5)
-        for entry in audit_log["audit_log_entries"]:
-            if int(entry["target_id"]) == int(guild_ban.user.id):
-                mod_user: interactions.User = await interactions.get(self.client, interactions.User, object_id=entry["user_id"])
-                if not mod_user.bot:
-                    channel = await interactions.get(self.client, interactions.Channel, object_id=logs_channel)
-                    await channel.send(f"**{guild_ban.user.username}#{guild_ban.user.discriminator} a été banni par {mod_user.username}#{mod_user.discriminator} pour la raison suivante:**\n{entry['reason']}")
-                return
+    async def on_guild_ban_add(self, _):
+        audit_logs = await self.guild.get_latest_audit_log_action(interactions.AuditLogEvents.MEMBER_BAN_ADD)
+        audit_log_entry = audit_logs.audit_log_entries[0]
+        mod_user = interactions.search_iterable(audit_logs.users, lambda user: int(user.id) == int(audit_log_entry.user_id))[0]
+
+        if not mod_user.bot:
+            banned_user = interactions.search_iterable(audit_logs.users, lambda user: int(user.id) == int(audit_log_entry.target_id))[0]
+            await self.logs_channel.send(embeds=create_info_embed(
+                f"**{banned_user.username}#{banned_user.discriminator} a été banni par {mod_user.mention} pour la raison suivante:**\n{audit_log_entry.reason}"
+            ))
 
     @interactions.extension_listener()
     async def on_message_create(self, message: interactions.Message):
