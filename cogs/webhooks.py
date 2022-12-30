3c84bd9407d95fb7d9ecf8b78f598d77600ab005
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2022-12-30 22:14:08 +0100

    more webhook testing

--- cogs/webhooks.py
+++ cogs/webhooks.py
@@ -24,8 +24,7 @@ WEBHOOKS = {
                 ("💬 L'utilisation de canaux qui s'écartent de leur but n'est pas autorisée.", "Veuillez lire la description du canal avant de chatter afin d'éviter toute sanction.", False),
                 ("📓 Eviter toute punition est fortement interdit.", " ⁣⁣", False),
                 ("♾️ Il est interdit d'exploiter une faille des règles.", "Le bon sens général s'applique à toutes les règles et doit être signalé immédiatement", False)
-            ],
-            include_thumbnail=False
+            ]
         )}, {"embeds": create_embed(
             title="🇬🇧 **Rules** 🇬🇧",
             color=0xFF0000,
@@ -44,8 +43,7 @@ WEBHOOKS = {
                 ("💬 Usage of channels that deviate from their purpose is not allowed.", "Please read the channel description before chatting to avoid punishment by Staff.", False),
                 ("📓 Evading any punishment is heavily forbidden.", " ⁣⁣", False),
                 ("♾️ Exploiting rule loopholes is also not allowed.", "General common sense applies to all rules, and must be reported immediately.", False)
-            ],
-            include_thumbnail=False
+            ]
         )}],
     "Accueil": [
         {"embeds": [
@@ -54,15 +52,13 @@ WEBHOOKS = {
                 description=f"""Le projet est simple, construire la planète Terre à l'échelle 1:1 sur Minecraft.
                 Le serveur est dédié à la construction de la France métropolitaine et de ses DOM-TOM.
 
-                Si vous souhaitez rejoindre le serveur en tant que builder ou bien pour visiter, toutes les étapes sont dans <#{variables.Channels.COMMENT_REJOINDRE}>""",
-                include_thumbnail=False
+                Si vous souhaitez rejoindre le serveur en tant que builder ou bien pour visiter, toutes les étapes sont dans <#{variables.Channels.COMMENT_REJOINDRE}>"""
             ), create_embed(
                 title="🇬🇧 Welcome to the BTE France Discord server 🇬🇧",
                 description=f"""The project is simple, build the planet Earth at a 1:1 scale on Minecraft.
                 The server is dedicated to the construction of metropolitan France and its overseas departments and territories.
 
-                If you want to join our server as a builder or just to visit, you must follow the steps in <#{variables.Channels.HOW_TO_JOIN}>.""",
-                include_thumbnail=False
+                If you want to join our server as a builder or just to visit, you must follow the steps in <#{variables.Channels.HOW_TO_JOIN}>."""
             )
         ]},
         {
@@ -72,7 +68,6 @@ WEBHOOKS = {
                 description="""⚠️ **Vous devez choisir votre langage pour avoir accès à l'entièreté du serveur!**
 
                 ⚠️ **You must choose your language to get access to the entirety of the server!**""",
-                include_thumbnail=False
             ), "components": [
                 interactions.Button(custom_id="verify_french", style=interactions.ButtonStyle.PRIMARY, label="Français", emoji=interactions.Emoji(name="🇫🇷")),
                 interactions.Button(custom_id="verify_english", style=interactions.ButtonStyle.PRIMARY, label="English", emoji=interactions.Emoji(name="🇬🇧"))
@@ -81,18 +76,66 @@ WEBHOOKS = {
     ],
     "IP": [
         {
-            "embeds": create_embed(
-                title="**Comment rejoindre le serveur BTE France ?**",
-                color=0x0000FF,
+            "embeds": [create_embed(
+                title="🇫🇷 **IP BTE France** 🇫🇷",
                 fields=[
-                    ("__IPs JAVA__", "(Version PC)", False),
-                    ("<:minecraft_heart:922583879469117491>", "BTE Hub : ```buildtheearth.net``` __Puis faites une fois en jeu :__```/bt FR```", True),
-                    ("<:minecraft_heart:922583879469117491>", "BTE France :```btefrance.fr```", True),
-                    ("__IP BEDROCK__", "(Windows 10, mobile, consoles)", False),
-                    ("<:bedrock:922583660761333850>", "```bedrock.buildtheearth.net, port: 19132```", True)
+                    ("Version Java", f"_Il vous faudra télécharger le modpack BTE pour accéder au serveur. Les liens d'installation sont dans <#{variables.Channels.COMMENT_REJOINDRE}>._\n**IP: btefrance.fr**", False),
+                    ("Version Bedrock", "_Il est actuellement impossible d'accéder au serveur via une version Bedrock du jeu (mobile, W10, console, ...)_", False)
                 ]
-            )
+            ), create_embed(
+                title="🇬🇧 **BTE France IP** 🇬🇧",
+                fields=[
+                    ("Java Version", f"_You will need to download the BTE modpack to access the server. The installation links can be found in <#{variables.Channels.HOW_TO_JOIN}>._\n**IP: btefrance.fr**", False),
+                    ("Bedrock Version", "_It is currently impossible to access the server using a Bedrock version of the game (mobile, W10, console, ...)_", False)
+                ]
+            )]
         }
+    ],
+    "Comment rejoindre?": [
+        {"embeds": [
+            create_embed(
+                title="🏦 Comment rejoindre le projet? 🏦",
+                description="""➡️ Il faut que vous ayez lancé au moins une fois la version 1.12 de Minecraft.
+
+                ➡️ Puis, il vous faudra télécharger le modpack avec les liens ci-dessous:
+                - [Windows](https://s3.buildtheearth.net/public/installer/latest/BTEInstaller-windows.zip)
+                - [Linux](https://s3.buildtheearth.net/public/installer/latest/BTEInstaller-linux.AppImage)
+                - [MacOS](https://s3.buildtheearth.net/public/installer/latest/BTEInstaller-mac.dmg)
+                - [Universal](https://s3.buildtheearth.net/public/installer/latest/BTEInstaller-universal.jar)
+
+                Tout est automatique, dézippez le fichier, ouvrez-le et cliquez sur **Install**, vous trouverez dans votre launcher Minecraft un nouveau profil BTE **(⚠️ fermez votre Launcher pendant l'installation)**""",
+            ),
+            create_embed(
+                title="⚙️ Quelle est l'IP? ⚙️",
+                description=f"➡️ Vous trouverez l'IP dans <#{variables.Channels.IP}> une fois avoir certifié la lecture des salons.",
+            ),
+            create_embed(
+                title="⛏️ Comment fonctionne le serveur? ⛏️",
+                description="""➡️ Accessible à la visite.
+                ➡️ Pour commencer à construire, demandez le grade `Débutant` **sur le serveur Minecraft à un membre du staff.**
+                ➡️ Pour devenir builder officiel (pas obligatoire), suivez [**ce document**](https://docs.google.com/document/d/1DHMOEcmepY_jGlS_-tvCvpJmSbaoHmofnamTJleQYik/edit?usp=sharing)"""
+            ),
+            create_embed(
+                title="👍 Comment bien débuter? 👍",
+                description="""➡️ Avoir le modpack (le lien juste au dessus)
+                ➡️ Se repérer grâce à Terramap, touche M du clavier en jeu
+                ➡️ Avoir Google Earth Pro afin de pouvoir mesurer les bâtiments en 3D
+                ➡️ Visiter le serveur pour voir comment chaque builder a construit son bâtiment. (`/visite` ou `/warps` sur le serveur)""",
+            ),
+            create_embed(
+                title="📍 Comment se téléporter? 📍",
+                description="""➡️ `/tpll [coordonnées]` (réservé aux builders)
+                ➡️ `/visite` pour nos plus belles constructions
+                ➡️ `/warps` pour tout voir
+                Utiliser `/tpll` ⤵️""",
+                image="https://s10.gifyu.com/images/TPLL_Help.gif"
+            ),
+            create_embed(
+                title="‼️ Il est impératif de lire ce salon avant de poser une question, merci ‼️",
+                description=f"Pour accéder à l'entièreté du Discord, n'oubliez pas de sélectionner votre langue dans <#{variables.Channels.ACCUEIL}>.",
+                color=0xFF0000
+            )
+        ]}
     ]
 }
 
@@ -126,85 +169,19 @@ class Webhooks(interactions.Extension):
     async def on_verify_french(self, ctx: interactions.ComponentContext):
         if variables.Roles.FRANCAIS in ctx.author.roles:
             await self.guild.remove_member_role(variables.Roles.FRANCAIS, ctx.author)
-            await ctx.send("Vous n'avez plus le rôle Français.")
+            await ctx.send("Vous n'avez plus le rôle Français.", ephemeral=True)
         else:
             await self.guild.add_member_role(variables.Roles.FRANCAIS, ctx.author)
-            await ctx.send("Vous avez reçu le rôle Français. Bienvenue sur le serveur!")
+            await ctx.send("Vous avez reçu le rôle Français. Bienvenue sur le serveur!", ephemeral=True)
 
     @interactions.extension_component("verify_english")
     async def on_verify_english(self, ctx: interactions.ComponentContext):
         if variables.Roles.ENGLISH in ctx.author.roles:
             await self.guild.remove_member_role(variables.Roles.ENGLISH, ctx.author)
-            await ctx.send("You lost the English role.")
+            await ctx.send("You lost the English role.", ephemeral=True)
         else:
             await self.guild.add_member_role(variables.Roles.ENGLISH, ctx.author)
-            await ctx.send("You received the English role. Welcome on the server!")
-
-    @interactions.extension_command(name="embeds", description="Mets à jour les embeds du serveur", default_member_permissions=interactions.Permissions.ADMINISTRATOR)
-    @interactions.option("Channel à mettre à jour", channel_types=[interactions.ChannelType.GUILD_TEXT])
-    async def _embeds(self, ctx: interactions.CommandContext, channel: interactions.Channel):
-        embeds = self.embeds.get(int(channel.id))
-
-        if not embeds:
-            await ctx.send(embeds=create_error_embed(f"{channel.mention} n'a pas d'embed correspondant!"), ephemeral=True)
-            return
-
-        await channel.send(**embeds)
-        await ctx.send(embeds=create_info_embed(f"L'embed a bien été mis à jour dans {channel.mention}"), ephemeral=True)
-
-    def ip_embed(self) -> dict:
-        IP = [
-            create_embed(
-                title="**Comment rejoindre le serveur BTE France ?**",
-                color=0x0000FF,
-                fields=[
-                    ("__IPs JAVA__", "(Version PC)", False),
-                    ("<:minecraft_heart:922583879469117491>", "BTE Hub : ```buildtheearth.net``` __Puis faites une fois en jeu :__```/bt FR```", True),
-                    ("<:minecraft_heart:922583879469117491>", "BTE France :```bte.thesmyler.fr```", True),
-                    ("__IP BEDROCK__", "(Windows 10, mobile, consoles)", False),
-                    ("<:bedrock:922583660761333850>", "```bedrock.buildtheearth.net, port: 19132```", True)
-                ]
-            )
-        ]
-        return {"embeds": IP}
-
-    def comment_rejoindre_embed(self) -> dict:
-        ip = interactions.Channel(id=variables.Channels.IP).mention
-        verify = interactions.Channel(id=variables.Channels.VERIFY).mention
-        COMMENT_REJOINDRE = [
-            create_embed(
-                title=":bank: Comment rejoindre le projet? :bank:",
-                description=":arrow_right: Il vous faut premièrement le modpack, à télécharger ci-dessous **(INUTILE POUR BEDROCK)**\nTout est automatique, dézippez le fichier, ouvrez-le et cliquez sur **Install**, vous le retrouverez dans votre launcher Minecraft **(:warning: fermez votre launcher pendant l'installation)**",
-                fields=[
-                    ("Liens d'installation:", "[Windows](https://s3.buildtheearth.net/public/installer/v1.20/BTEInstaller-1.20.0-windows.zip)\n[Linux](https://s3.buildtheearth.net/public/installer/v1.20/BTEInstaller-1.20.0-linux.AppImage)\n[Mac](https://s3.buildtheearth.net/public/installer/v1.20/BTEInstaller-1.20.0-mac.dmg)\n[Universal](https://s3.buildtheearth.net/public/installer/v1.20/BTEInstaller-1.20.0-universal.jar)", False)
-                ]
-            ),
-            create_embed(
-                title=":gear: Quelle est l'IP? :gear:",
-                description=f":arrow_right: Vous trouverez l'IP dans {ip} une fois avoir certifié la lecture des salons dans {verify}.",
-            ),
-            create_embed(
-                title=":pick: Comment fonctionne le serveur? :pick:",
-                description=":arrow_right: Accessible à la visite.\n:arrow_right: Pour construire, demandez le grade __Débutant__ **SUR le serveur Minecraft à un membre du staff.**",
-                fields=[
-                    ("Devenir builder officiel (pas obligatoire) :arrow_heading_down:", "[**Document**](https://docs.google.com/document/d/1DHMOEcmepY_jGlS_-tvCvpJmSbaoHmofnamTJleQYik/edit?usp=sharing)", False)
-                ]
-            ),
-            create_embed(
-                title=":thumbsup: Comment bien débuter? :thumbsup:",
-                description=":arrow_right: Avoir le modpack (le lien juste au dessus)\n:arrow_right: Se repérer grâce à Terramap, touche M du clavier en jeu\n:arrow_right: Avoir Google Earth Pro afin de pouvoir mesurer les bâtiments en 3D\n:arrow_right: Visiter le serveur pour voir comment chaque builder a construit son bâtiment. (`/visite` ou `/warps` sur le serveur)",
-            ),
-            create_embed(
-                title=":round_pushpin: Comment se téléporter? :round_pushpin:",
-                description=":arrow_right: `/tpll [coordonnées]` (réservé aux builders)\n:arrow_right: `/visite` pour nos plus belles constructions\n:arrow_right: `/warps` pour tout voir\n\nUtiliser `/tpll` :arrow_heading_down:",
-                image="https://s10.gifyu.com/images/TPLL_Help.gif"
-            ),
-            create_embed(
-                title=":bangbang: Il est impératif de lire ce salon avant de poser une question, merci :bangbang:",
-                description=f"Pour accéder à l'entièreté du Discord, n'oubliez pas de cliquez sur le :white_check_mark: dans {verify}.\n\n:red_square: **VERSIONS JAVA ET BEDROCK OFFICIELLES SEULEMENT** :red_square:",
-            )
-        ]
-        return {"embeds": COMMENT_REJOINDRE}
+            await ctx.send("You received the English role. Welcome on the server!", ephemeral=True)
 
 
 def setup(client: interactions.Client):
