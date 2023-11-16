a099ac5b1289f0a338600d19c1c60a7bc0d5adbe
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2023-11-16 21:57:33 +0100

    add dynamic staff list in /mc command

--- cogs/mcserv.py
+++ cogs/mcserv.py
@@ -1,40 +1,41 @@
+import json
+import os
 import socket
 
 import interactions
 from mcstatus import JavaServer
 
-from utils import create_embed, escape_minecraft_username_markdown
+from utils import create_embed, escape_minecraft_username_markdown, log
 
-STAFF_USERNAMES = [
-    "lennah_",
-    "*maxdrey49",
-    "bahlkog",
-    "l0pal",
-    "luckylukass",
-    "madin0",
-    "maxyolo01",
-    "serenityjane",
-    "weelly_",
-    "eklo",
-    "egan83",
-    "babyoda_",
-    "maxdrey",
-    "smyler_",
-    "thesmyler",
-    "__elfi__",
-    "litverkzad",
-    "repsic",
-    "zxsp3ctrom",
-    "tytarex",
-    "lclc",
-    "azguendare",
-    "dstarmc",
-]
 SERVER_IP = "btefr.thesmyler.fr:7011"
 SERVER_DESC = "Serveur Java 1.20.1 - IP: btefrance.fr"
 
 
 class MCServ(interactions.Extension):
+    @interactions.listen(interactions.events.Startup)
+    async def on_start(self):
+        self.staff_players = []
+        if users_json_file := os.getenv("LUCKPERMS_USERS_JSON_FILE"):
+            self.users_json_file = users_json_file
+            self.staff_players = []
+            await self.update_staff_players()
+            self.update_staff_players.start()
+        else:
+            log("No LUCKPERMS_USERS_JSON_FILE variable found!")
+
+    @interactions.Task.create(interactions.IntervalTrigger(minutes=1))
+    def update_staff_players(self):
+        """Find staff players that have helper or above role using the LuckPerms file"""
+        self.staff_players = []
+        with open(self.users_json_file, "r") as file:
+            users = json.load(file)
+            for user_dict in users.values():
+                name = user_dict["name"]
+                for group in user_dict["parents"]:
+                    if group["group"] in ("admin", "dev", "helper"):
+                        self.staff_players.append(name.lower())
+                        break
+
     @interactions.slash_command(name="mc")
     async def mc(self, ctx: interactions.SlashContext):
         "Statut du serveur Minecraft"
@@ -55,7 +56,7 @@ class MCServ(interactions.Extension):
         else:
             staff, players = [], []
             for player in query.players.names:
-                if player.lower() in STAFF_USERNAMES:
+                if player.lower() in self.staff_players:
                     staff.append(f"**{escape_minecraft_username_markdown(player)}**")
                 else:
                     players.append(escape_minecraft_username_markdown(player))
