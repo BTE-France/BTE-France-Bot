b9b398f157249ef5233d7d74561adcbbe8b5cfdf
mAxYoLo01 <maxyolo01.ytb@gmail.com>
2026-02-11 22:53:00 +0100

    Make builder tickets not invitable

--- cogs/ticket.py
+++ cogs/ticket.py
@@ -184,7 +184,9 @@ class Ticket(interactions.Extension):
                 ephemeral=True,
             )
 
-        thread = await ctx.channel.create_private_thread(name=thread_name, auto_archive_duration=interactions.AutoArchiveDuration.ONE_WEEK)
+        thread = await ctx.channel.create_private_thread(
+            name=thread_name, invitable=False, auto_archive_duration=interactions.AutoArchiveDuration.ONE_WEEK
+        )
         await ctx.send(
             embed=create_info_embed(f"Demande créée ({thread.mention})" if fr else f"Application created ({thread.mention})"),
             ephemeral=True,
